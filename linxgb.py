faa127947048146724f73f8c9d608f55e2735271
laurent <devitolaurent@gmail.com>
2017-09-11 21:33:49 +0200

    feat: LinXGBoost
    The user manipulates only a linxgb instance defined in linxgb.py.
    It builds trees recursively: A tree is a node as defined
    in node.py.

---
+++ linxgb.py
@@ -0,0 +1,224 @@
+import sys
+import numpy as np
+from node import node
+
+def make_quadratic(X):
+    assert isinstance(X, np.ndarray), "X must be a numpy ndarray!"
+    return np.c_[ X, X*X ]
+
+class linxgb:
+    def __init__(self, loss_func="square_loss", n_estimators=5,
+                 min_samples_split=3, min_samples_leaf=2, max_depth=6,
+                 max_samples_linear_model=sys.maxsize,
+                 subsample=1.0,
+                 learning_rate=0.3, min_split_loss=0.0, gamma=0.0, lbda=0.0,
+                 prune=True,
+                 random_state=None,
+                 verbose=0, nthread=1):
+
+        self.loss_func = loss_func
+        self.n_estimators = n_estimators
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.max_depth = max_depth
+        self.max_samples_linear_model = max_samples_linear_model
+        self.subsample = subsample
+        self.learning_rate = learning_rate
+        self.min_split_loss = min_split_loss
+        self.lbda = lbda
+        self.gamma = gamma
+        self.prune = prune
+        self.random_state = random_state
+        self.verbose = verbose
+        self.nthread = nthread
+
+        self.check_params()
+
+    def get_params(self, deep=True):
+        return self.__dict__
+
+    def set_params(self, **params):
+        if not params:
+            return self
+        valid_params = self.get_params(deep=True)
+        for key in params.keys():
+            if key not in valid_params:
+                    raise ValueError('Invalid parameter %s for estimator %s. '
+                                     'Check the list of available parameters '
+                                     'with `estimator.get_params().keys()`.' %
+                                     (key, self.__class__.__name__))
+            setattr(self, key, params[key])
+        return self
+
+    def check_params(self):
+        """Check validity of parameters and raise ValueError if not valid. """
+        if self.n_estimators <= 0:
+            raise ValueError("n_estimators must be greater than 0 but "
+                             "was %r" % self.n_estimators)
+
+        if self.learning_rate <= 0.0:
+            raise ValueError("learning_rate must be greater than 0 but "
+                             "was %r" % self.learning_rate)
+
+        if self.lbda < 0.0:
+            raise ValueError("lbda must be greater than 0 but "
+                             "was %r" % self.lbda)
+
+        if self.gamma < 0.0:
+            raise ValueError("gamma must be greater than 0 but "
+                             "was %r" % self.gamma)
+
+    def predict(self, X):
+        n = X.shape[0]
+        y = np.zeros(n, dtype=float)
+        for tree in self.trees:
+            y += self.learning_rate*tree.predict(X)
+
+        return y
+
+    def loss(self, y, y_hat):
+        return np.sum(np.square(y_hat-y))
+
+    def dloss(self, X, y, y_hat):
+        n = len(y)
+        return 2*(y_hat-y), 2*np.ones(n, dtype=float)
+
+    def regularization(self):
+        reg = 0.
+        for tree in self.trees:
+            reg += tree.regularization(gamma=self.gamma, lbda=self.lbda)
+
+        return reg
+
+    def objective(self,X, y, y_hat=None):
+        if y_hat is None:
+            y_hat = self.predict(X)
+        return self.loss(y,y_hat)+self.regularization()
+
+    def build_tree(self, tree, X, g, h):
+        assert tree.left == None, "the node must be a leaf!"
+
+        n, d = X.shape
+        linear_model = ( n > d ) and ( n <= self.max_samples_linear_model )
+        try:
+            tree.set_weight(X, g, h, self.lbda, linear_model)
+        except:
+            print( "in tree building: something went wrong!" )
+            raise
+        if tree.depth >= self.max_depth:
+            return tree
+        if n < self.min_samples_split:
+            return tree
+        tree.find_best_split(X, g, h, self.lbda, self.gamma, self.max_samples_linear_model, self.min_samples_leaf)
+        if tree.split_feature == -1: # no split because of the constraints
+            if self.verbose > 2:
+                print( "node could not be split" )
+            return tree
+        left_child, right_child = tree.add()
+        c = ( X[:,tree.split_feature] < tree.split_value )
+        if self.verbose > 1:
+            print( "creating left child with {:d} instances".format(np.sum(c)) )
+        try:
+            self.build_tree(left_child, X[c,:], g[c], h[c])
+        except RuntimeError:
+            print( "maximum recursion depth exceeded: Tree depth is {}".format(tree.depth) )
+            tree.left = None
+            tree.right = None
+            return tree
+            raise
+
+        c = np.invert(c)
+        if self.verbose > 1:
+            print( "creating right child with {:d} instances".format(np.sum(c)) )
+        try:
+            self.build_tree(right_child, X[c,:], g[c], h[c])
+        except RuntimeError:
+            print( "maximum recursion depth exceeded: Tree depth is {}".format(tree.depth) )
+            tree.left = None
+            tree.right = None
+            return tree
+            raise
+
+        return tree
+
+    def fit(self, X, y):
+        if y.ndim != 1:
+            print( "lingxb.fit() is expecting a 1D array!" )
+            y = y.ravel()
+        assert X.shape[0] == len(y)
+
+        self.trees = []
+        self.tree_objs = []
+
+        if self.random_state is not None:
+            np.random.seed(self.random_state)
+
+        self.tree_objs.append( self.objective(X,y) )
+
+        for t in range(0,self.n_estimators):
+            n = X.shape[0]
+            batch_size = int(np.rint(self.subsample*n))
+            indices = np.random.choice(n, batch_size, replace=False)
+            y_hat = self.predict(X[indices,:])
+            g, h = self.dloss(X[indices,:],y[indices],y_hat)
+            if self.verbose > 0:
+                print( "building tree {}, total obj={}".format(t+1,np.sum(self.tree_objs)) )
+            tree = self.build_tree( node(verbose=self.verbose), X[indices,:], g, h )
+            self.trees.append(tree)
+            tree_obj = tree.objective(self.gamma)
+            if self.verbose > 0:
+                print( "tree max. depth={}, num. of leaves={}, obj_{}={}, total obj={}". \
+                       format(tree.max_depth(),tree.num_leaves(), t+1, tree_obj, np.sum(self.tree_objs)+tree_obj) )
+            # pruning
+            if self.prune:
+                num_pruning = self.prune_tree_type_2(tree)
+                if num_pruning > 0:
+                    tree_obj = tree.objective(self.gamma)
+                    if self.verbose > 0:
+                        print("{} nodes pruned, ".format(num_pruning))
+                        print( "tree max. depth={}, num. of leaves={}, obj_{}={}, total obj={}". \
+                               format(tree.max_depth(),tree.num_leaves(), t+1, tree_obj, np.sum(self.tree_objs)+tree_obj) )
+            self.tree_objs.append(tree_obj)
+
+            # check if the objective of the tree is positive
+            if tree_obj > 0.:
+                if self.verbose > 0:
+                    print( "the objective of tree {}/{} of depth {} is positive: obj = {:.4e}". \
+                            format(t+1,self.n_estimators,tree.max_depth(),tree_obj) )
+                del self.trees[-1]
+                del self.tree_objs[-1]
+                break
+
+    def prune_tree_type_1(self, tree):
+        num_pruning = 0
+
+        if not tree.is_leaf():
+            if not tree.left.is_leaf():
+                num_pruning += self.prune_tree_type_1(tree.left)
+            if not tree.right.is_leaf():
+                num_pruning += self.prune_tree_type_1(tree.right)
+            if tree.left.is_leaf() and tree.right.is_leaf():
+                if tree.gain < 0.:
+                    if self.verbose > 1:
+                        print( "pruning at depth {}".format(tree.depth) )
+                    tree.left = None
+                    tree.right = None
+                    num_pruning += 1
+
+        return num_pruning
+
+    def prune_tree_type_2(self, tree):
+        num_pruning = 0
+
+        if not tree.is_leaf():
+            if tree.gain < 0.:
+                if tree.obj < tree.objective(gamma=self.gamma):
+                    # undo the split
+                    tree.left = None
+                    tree.right = None
+                    num_pruning += 1
+            if not tree.is_leaf():
+                num_pruning += self.prune_tree_type_2(tree.left)
+                num_pruning += self.prune_tree_type_2(tree.right)
+
+        return num_pruning
---
+++ node.py
@@ -0,0 +1,159 @@
+import numpy as np
+
+class node:
+    def __init__(self, verbose=0):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.split_value = 0.0
+        self.split_feature = -1
+        self.w = 0
+        self.depth = 1
+        self.gain = 0
+        self.obj = 0
+        self.verbose = verbose
+
+    def is_leaf(self):
+        return (self.left == None)
+
+    def max_depth(self):
+        if self.is_leaf():
+            return self.depth
+        else:
+            return max( self.left.max_depth(), self.right.max_depth() )
+
+    def num_leaves(self):
+        if self.is_leaf():
+            return 1
+        else:
+            return self.left.num_leaves()+self.right.num_leaves()
+
+    def add(self):
+        assert self.is_leaf(), "add children to a leaf!"
+
+        left_node = node(verbose=self.verbose)
+        left_node.parent_id = self
+        left_node.depth = self.depth+1
+        self.left = left_node
+
+        right_node = node(verbose=self.verbose)
+        right_node.parent_id = self
+        right_node.depth = self.depth+1
+        self.right = right_node
+
+        return self.left, self.right
+
+    def delete_node(self):
+        assert self.is_leaf(), "delete a leaf!"
+        assert self.depth != 1, "cannot delete root!"
+
+        parent = self.parent
+        parent.left = None
+        parent.right = None
+
+        return parent
+
+    def objective(self, gamma=0.):
+        if self.is_leaf():
+            return self.obj+gamma
+        else:
+            return self.left.objective(gamma)+self.right.objective(gamma)
+
+    def regularization(self, gamma=0., lbda=0.):
+        if self.is_leaf():
+            if np.isscalar(self.w):
+                #w2 = self.w*self.w
+                w2 = 0.
+            else:
+                w2 = np.dot(self.w,self.w)
+            return gamma + 0.5*lbda*w2
+        else:
+            return self.left.regularization(gamma,lbda)+self.right.regularization(gamma,lbda)
+
+    def predict(self,X):
+        n = X.shape[0]
+        if self.is_leaf():
+            if np.isscalar(self.w):
+                return self.w
+            else:
+                return np.dot(np.c_[X,np.ones(shape=(n,1), dtype=float)],self.w)
+        else:
+            assert self.split_feature > -1, "split feature must be > 0!"
+            y = np.zeros(n, dtype=float)
+            c = ( X[:,self.split_feature] < self.split_value )
+            y[c]            = self.left.predict(X[c,:])
+            y[np.invert(c)] = self.right.predict(X[np.invert(c),:])
+            return y
+
+    def find_best_split(self, X, g, h, lbda, gamma, max_samples_linear_model, min_samples_leaf):
+        assert X.shape[0] == len(g)
+        assert len(h) == len(g)
+        assert self.split_feature == -1
+
+        n, d = X.shape
+        self.gain = float("-inf")
+        for f in range(0,d): # split feature
+            var = np.sort(np.unique(X[:,f]))
+            mid_var = [ (x+y)/2. for x,y in zip(var[::],var[1::]) ]
+            for pos in range(0,len(mid_var)): # split value
+                c = ( X[:,f] < mid_var[pos] )
+                left_n = np.sum(c)
+                right_n = n-left_n
+                if ( left_n < min_samples_leaf ) or ( right_n < min_samples_leaf ):
+                    continue
+                linear_model = ( left_n > d ) and ( left_n <= max_samples_linear_model )
+                try:
+                    _ , obj_left = self.get_weight( X[c,:], g[c], h[c], lbda, linear_model)
+                except:
+                    if self.verbose > 1:
+                        print( "exception when testing for split of feature {} at pos {}".format(f,pos) )
+                    continue
+                c = np.invert(c)
+                linear_model = ( right_n > d ) and ( right_n <= max_samples_linear_model )
+                try:
+                    _ , obj_right = self.get_weight( X[c,:], g[c], h[c], lbda, linear_model)
+                except:
+                    if self.verbose > 1:
+                        print( "exception when testing for split of feature {} at pos {}".format(f,pos) )
+                    continue
+                gain = self.obj-(obj_left+obj_right+gamma)
+                if gain > self.gain:
+                    self.gain = gain
+                    self.split_feature = f
+                    self.split_value = mid_var[pos]
+
+        if self.verbose > 1:
+            if ( self.gain != float("-inf") ) and ( self.gain < 0. ):
+                print( "negative gain!" )
+            print( "find best split: gain={:+6.4e}, feature={:2d}, value={:+8.4f}".format(self.gain, self.split_feature, self.split_value) )
+
+    def set_weight(self, X, g, h, lbda, linear_model):
+        self.w, self.obj = self.get_weight(X, g, h, lbda, linear_model)
+
+    def get_weight(self, X, g, h, lbda, linear_model):
+        if linear_model:
+            n,d = X.shape
+            X_tilde = np.c_[X,np.ones(shape=(n,1), dtype=float)]
+            g_tilde = np.dot(X_tilde.transpose(),g)
+            H_tilde = np.dot(X_tilde.transpose()*h, X_tilde)
+            #C = H_tilde+lbda*np.eye(d+1)
+            Lambda = lbda*np.eye(d+1)
+            Lambda[d,d] = 0.
+            C = H_tilde+Lambda
+            try:
+                L = np.linalg.cholesky(C)
+            except:
+                if self.verbose > 1:
+                    print( "C is not definite positive (X has {} instances)!".format(n) )
+                return self.get_weight(X, g, h, lbda, linear_model=False)
+                raise
+            w = -np.linalg.solve(L.transpose(), np.linalg.solve(L,g_tilde))
+            obj = 0.5*np.dot(g_tilde,w)
+        else:
+            g_tilde = np.sum(g)
+            H_tilde = np.sum(h)
+            #w = -g_tilde/(lbda+H_tilde)
+            w = -g_tilde/H_tilde
+            obj = 0.5*g_tilde*w
+
+        return w, obj
